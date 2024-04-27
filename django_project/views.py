12d4e3e202d3843835aa2828643eab078a104c8f
Anuoluwaplp4nm <159920744+Anuoluwaplp4nm@users.noreply.github.com>
2024-04-27 21:45:19 +0000

    Updated index.html, styles.css and view.py file

--- django_project/views.py
+++ django_project/views.py
@@ -1,14 +1,17 @@
 import requests
 from django.shortcuts import render
 
+
 def home(request):
   #USING APIs
-  response = requests.get('https://api.github.com/events')
+  response = requests.get('https://www.themealdb.com/api/json/v1/1/random.php')
   data = response.json()
-  result = data[0]['repo']
+  result = data['meals']
 
-  response2 = requests.get('https://dog.ceo/api/breeds/image/random')
-  data2 = response2.json()
-  result2 = data2['message']
-  
-  return render(request, 'templates/index.html', {'result': result, 'result2': result2})
+  #response2 = requests.get('https://api.puppydeveloper.com/food')
+  #data2 = response2.json()
+  #result2 = data2['meals']
+
+  return render(request, 'templates/index.html', {
+      'result': result,
+  })
